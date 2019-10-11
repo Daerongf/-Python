0846f29e1b43b4403f6f90930259d77f9d7e89fc
Daerongf <daerongf@gmail.com>
2019-10-11 21:38:43 +0600

    Закончил 2 задачу про вывод заглавных букв, у которых 2 слева строчные и 2 справа заглавные. Если читать задание как "РОВНО две", а не "ХОТЯ БЫ две", то результат получается отличным от результата регулярки и от описанного в задании результата. Очевидно, они работают по-другому.

--- Lesson4/HW4_normal.py
+++ Lesson4/HW4_normal.py
@@ -68,28 +68,65 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
 str2 = re.findall(r'[a-z|а-я]{2}([A-Z|А-Я]+)[A-Z|А-Я]{2}', line_2)
 print(str2)
 
-# line_3 = 'asDfgHJk'
-# task2 = []
-# task22[]
-# str22 = ""
-# cnt = 0
-# for i in line_2:
-#        if i.isupper():
-#               str22 += i
-#               # print(str22)
-#
-#        elif str22:
-#               task2.append(str22)
-#               str22 = ""
-# if str22 != "":
+task2 = []
+task22 = []
+str22 = ""
+cnt = []
+i = 0
+# print(len(line_2))
+print("Читаем задание как \"РОВНО две строчных буквы перед и РОВНО две заглавных после\" (регулярка работает не так):" )
+for i in range(len(line_2)-2):
+    if i == 0 and line_2[i].islower() and line_2[i+1].islower() and line_2[i+2].isupper():
+        cnt.append(i + 2)
+    elif line_2[i].isupper() and line_2[i+1].islower() and line_2[i+2].islower() and line_2[i+3].isupper():
+        cnt.append(i + 3)
+    # elif i == 0
+# print(cnt)
+
+j = 0
+for j in cnt:
+       try:
+              while line_2[j].isupper():
+                     str22 += line_2[j]
+                     # print(str22)
+                     j += 1
+                     if j == cnt[-1]:
+                            break
+       except IndexError:
+              pass
+       if str22:
+              task2.append(str22)
+              str22 = ""
+
+
+
+
+# for j in range(len(line_2)):
+#        while line_2[j].isupper() and j in cnt: #and (i-1).islower:   Тут надо или Уайл попробовать, или уайл. изаппер. И засунуть туда куски начиная с номеров j in cnt
+#               str22 += line_2[j]
+#               j += 1
+#               if line_2[j].islower():
+#                      break
 #        task2.append(str22)
-#
-# for j in range(len(task2)):
-#        if len(task2[j]) > 2:
-#               task2[j] =  ======Не успел. Доделать!!!=======
+              # print(str22)
 
 
-print(task2)
+       # elif str22:
+       #        task2.append(str22)
+       #        str22 = ""
+if str22 != "":
+       task2.append(str22)
+# print(task2)
+k = 0
+for k in range(len(task2)):
+       if len(task2[k]) > 2:
+              task2[k] = task2[k][:-2]
+              task22.append(task2[k])
+              # str222 = task2[j]
+
+
+print(task22)
+
 
 
 # Задание-3:
