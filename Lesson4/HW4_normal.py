aec4442f0aa0fd4a531315197ca91d2cd98ef469
Daerongf <daerongf@gmail.com>
2019-10-12 23:35:49 +0600

    Сделал 3 задачу и чуть-чуть поправил вторую, в которой остаются неразрешенные вопросы.

--- Lesson4/HW4_normal.py
+++ Lesson4/HW4_normal.py
@@ -71,12 +71,16 @@ print(str2)
 print("Если ровно два символа слева регулярным выражением:")
 
 if line_2[0].isupper():
-       str2 = re.findall(r'[A-Z|А-Я][a-z|а-я]{2}([A-Z|А-Я]+)[A-Z|А-Я]{2}', line_2)
+       str2 = re.findall(r'[A-Z|А-Я]+[a-z|а-я]{2}([A-Z|А-Я]+)[A-Z|А-Я]{2}', line_2)
 else:
-       str2 = str2 = re.findall(r'^[a-z|а-я]{2}([A-Z|А-Я]+)[A-Z|А-Я]{2}', line_2) + \
-                     re.findall(r'[A-Z|А-Я][a-z|а-я]{2}([A-Z|А-Я]+)[A-Z|А-Я]{2}', line_2)
+       str2 = re.findall(r'^[a-z|а-я]{2}([A-Z|А-Я]+)[A-Z|А-Я]{2}', line_2) + \
+                     re.findall(r'[A-Z|А-Я]+[a-z|а-я]{2}([A-Z|А-Я]+)[A-Z|А-Я]{2}', line_2)
 print(str2)
-
+print()
+print("Не понятно, почему не попадает символ Q из выражения ...HlmQKJn...\n "
+       "Вообще должен, и в решении без регулярки он есть. Причем если применить это регулярное выражение только к части\n"
+       "строки, например, HlmQKJndiA, символ Q в ответ выводится. Загадка!")
+print()
 task2 = []
 task22 = []
 str22 = ""
@@ -108,21 +112,6 @@ for j in cnt:
               str22 = ""
 
 
-
-
-# for j in range(len(line_2)):
-#        while line_2[j].isupper() and j in cnt: #and (i-1).islower:   Тут надо или Уайл попробовать, или уайл. изаппер. И засунуть туда куски начиная с номеров j in cnt
-#               str22 += line_2[j]
-#               j += 1
-#               if line_2[j].islower():
-#                      break
-#        task2.append(str22)
-              # print(str22)
-
-
-       # elif str22:
-       #        task2.append(str22)
-       #        str22 = ""
 if str22 != "":
        task2.append(str22)
 # print(task2)
@@ -131,7 +120,7 @@ for k in range(len(task2)):
        if len(task2[k]) > 2:
               task2[k] = task2[k][:-2]
               task22.append(task2[k])
-              # str222 = task2[j]
+
 
 
 print(task22)
@@ -144,3 +133,79 @@ print(task22)
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+print()
+print('Задача3')
+import os
+import random
+
+path = os.path.join('randomdigit.txt')
+digitstr = ""
+with open(path, 'w', encoding='UTF-8') as rnd:
+       i = 0
+       while i <= 2500:
+            rnddigit = random.randint(0,9)
+            digitstr += str(rnddigit)
+            # rnd.write()
+            i += 1
+       rnd.write(digitstr)
+
+with open(path, 'r', encoding='UTF-8') as rnd2:
+       stroka = rnd2.read()
+       print(stroka)
+j = 0
+item = 0
+eqlist = []
+equal = ""
+
+# print(stroka)
+
+if stroka[0] == stroka [1]:
+       equal += str(stroka[0])
+       if stroka[1] != stroka[2]:
+              equal += str(stroka[1])
+              eqlist.append(equal)
+              equal = ""
+
+for j in range(1, len(stroka)-1):
+     if stroka[j] == stroka[j+1]:
+            equal += str(stroka[j])
+     elif stroka[j] == stroka[j-1] and j != 1:
+            equal += str(stroka[j])
+            eqlist.append(equal)
+            equal = ""
+
+if stroka[-1] == stroka[-2]:
+       equal += str(stroka[-1])
+       eqlist.append(equal)
+       equal = ""
+
+if equal:
+       eqlist.append(equal)
+print("Повторяющиеся последовательности: ", eqlist)
+
+lenlist = list(map(len, eqlist))
+print("Длины повторяющихся последовательностей: ", lenlist)
+maxlen = max(lenlist)
+print("Максимальное количество повторяющихся подряд символов: ", maxlen)
+
+k = 0
+l = 0
+maxlenlist = []
+for l in eqlist:
+       if len(l) == maxlen:
+              maxlenlist.append(l)
+print('Самые длинные последовательности: ', maxlenlist)
+q = 0
+qq = ""
+for q in maxlenlist:
+      qq += q + " "
+
+with open(path, 'a', encoding='UTF-8') as rnd3:
+       rnd3.write("\n")
+       rnd3.write("\n")
+       rnd3.write(qq)
+
+
+
+
+
